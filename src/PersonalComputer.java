afdeef0621dbe04afd926471e62faa0b12349019
Michael Drougas <sehser5@hotmail.com>
2023-03-17 16:23:09 -0400

    Rearranged files and finished PersonalComputer class

--- src/PersonalComputer.java
+++ src/PersonalComputer.java
@@ -3,11 +3,25 @@ public class PersonalComputer extends Product {
     private Monitor monitor;
     private Motherboard motherboard;
 
-    public PersonalComputer(String model, String manufacturer, ComputerCase computerCase
-            , Monitor monitor, Motherboard motherboard) {
+    public PersonalComputer(String model, String manufacturer,
+                            ComputerCase computerCase, Monitor monitor,
+                            Motherboard motherboard) {
         super(model, manufacturer);
         this.computerCase = computerCase;
         this.monitor = monitor;
         this.motherboard = motherboard;
     }
+
+    public ComputerCase getComputerCase() {
+        return computerCase;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public Motherboard getMotherboard() {
+        return motherboard;
+    }
 }
+
